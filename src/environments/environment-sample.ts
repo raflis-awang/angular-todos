ba60e9eb278ee613c8aa550855ab7ec7b95aa711
Raflis Awang <dgmortal@gmail.com>
2020-04-21 19:50:56 +0800

    Update angular package. add env file to gitignore

---
+++ src/environments/environment-sample.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: { // Paste-in your firebase configuration here
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: ''
+  }
+};
