35cd010cd6f5e983972a812544936eff9fccfdb1
raflis-awang <dgmortal@gmail.com>
2020-04-21 19:54:04 +0800

    Hide configuration values

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyAhmT2HZAwjSW_7p0upN06yiycNtUcB0W8",
-    authDomain: "todo-app-95ffb.firebaseapp.com",
-    databaseURL: "https://todo-app-95ffb.firebaseio.com",
-    projectId: "todo-app-95ffb",
-    storageBucket: "todo-app-95ffb.appspot.com",
-    messagingSenderId: "125461221324"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
   }
 };
